605b4152cccdf614bcb24f8498a9fecc8bdb14a9
Larissa Marcich <lmarcich@thoughtworks.com>
2019-10-28 23:58:14 +0800

    [Lari] first pass at malicious csrf demo site

--- csrf/banking_app/src/main/java/demo/csrf/banking/HomeController.java
+++ csrf/banking_app/src/main/java/demo/csrf/banking/HomeController.java
@@ -3,7 +3,9 @@ package demo.csrf.banking;
 import demo.csrf.banking.model.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
 import java.util.LinkedList;
